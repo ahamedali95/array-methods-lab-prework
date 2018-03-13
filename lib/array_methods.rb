bf94cea561757907ade7e4575139e5593c1ae632
AHAMED ABBAS <ahamedali95@gmail.com>
2018-03-13 01:58:06 +0000

    automatically backed up by learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -4,32 +4,32 @@ def using_include(array, element)
   #   if array[i] == element
   #     return true
   #   end
-  # 
+  #
   #   i += 1
   #   break if i == array.length
   # end
-  # 
+  #
   # false
 
   array.include?(element)
 end
 
 def using_sort(array)
-
+  array.sort
 end
 
 def using_reverse(array)
-
+  array.reverse
 end
 
 def using_first(array)
-
+  array.first
 end
 
 def using_last(array)
-
+  array.last
 end
 
 def using_size(array)
-
+  array.length
 end
