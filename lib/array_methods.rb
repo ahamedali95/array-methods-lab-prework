e5dde225a4a900cf1f74438d51f18885d364d735
AHAMED ABBAS <ahamedali95@gmail.com>
2018-03-13 01:53:39 +0000

    automatically backed up by learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,5 +1,13 @@
 def using_include(array, element)
+  i = 0 
+  loop do
+    if array[i] == element
+      return true
+    end
 
+    i += 1
+    break if i == array.length
+  end
 end
 
 def using_sort(array)
